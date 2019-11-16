7e4a7a6d860419a9cf3be3fa11dcfa604b81f4ab
abbyabby33 <sk573129@gmail.com>
2019-11-17 02:55:21 +0800

    靜 1.客製明信片資料寫入dd103資料庫 2.購買後存入localStorage 3.完成後註銷左側步驟返回功能

---
+++ postcardToDb.php
@@ -0,0 +1,29 @@
+<?php 
+$errMsg ="";
+$postcardNo = $_POST['hidden_data3'];
+$memNo = $_POST['hidden_data4'];
+$postcardPic = $_POST['hidden_data5'];
+
+try {
+	$dsn = "mysql:host=localhost;port=3306;dbname=dd103g1;charset=utf8";
+	$user = "dd103g1";
+	$password = "dd103g1";
+	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
+	$pdo = new PDO( $dsn, $user, $password, $options);  
+	$sql = "INSERT INTO postcard(postcardNo, memNo, postcardPic) VALUES(:postcardNo, :memNo,:postcardPic)";
+
+	//INSERT INTO postcard(postcardNo, memNo, postcardPic) VALUES(1, 1,'m001p01')
+
+	$postcard = $pdo->prepare($sql);
+	$postcard->bindValue(":postcardNo", $postcardNo);
+	$postcard->bindValue(":memNo", $memNo);
+	$postcard->bindValue(":postcardPic", $postcardPic);
+
+	$postcard->execute();
+	echo "存入資料庫成功~~~目前只能存一筆~~~";
+} catch (PDOException $e) {
+	$errMsg .= "錯誤原因 : ".$e -> getMessage(). "<br>";
+  	$errMsg .= "錯誤行號 : ".$e -> getLine(). "<br>";
+  	echo $errMsg;
+}
+?> 
