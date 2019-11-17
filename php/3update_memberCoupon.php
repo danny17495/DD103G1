32618adbd6e114d021e8f1722fa0d46bb8f9185f
catcatgogo0923meimei0923 <catcatgogo0923@gamil.com>
2019-11-17 21:22:51 +0800

    mei  更改PHP為

---
+++ php/3update_memberCoupon.php
@@ -0,0 +1,20 @@
+<?php
+$errMsg="";
+
+try {
+    session_start();
+    require_once('connectBook.php');
+  $postcard=$pdo->prepare("UPDATE `coupon` SET `postcard_status` = '0' WHERE `postcard`.`postcardNo` = :postcardNo AND `postcard`.`postcardNo` = :postcardNo;");
+  $postcard->bindValue(':memNo',$_SESSION['memNo']);
+  $postcard->bindValue(':postcardNo',$_POST['postcardNo']);
+  $postcard->execute();
+  echo"異動成功";
+
+} catch (PDOException $e) {
+
+    $errMsg = $errMsg . "錯誤訊息: " . $e->getMessage() . "</br>";
+    $errMsg .= "錯誤行號: " . $e->getLine() . "<br>";
+    echo $errMsg;
+}
+
+?>
---
+++ php/4update_memberOrder.php
@@ -0,0 +1,35 @@
+<?php
+$errMsg="";
+session_start();
+
+try {
+  require_once('connectBook.php');
+
+  $orders=$pdo->prepare("select * from `orderform` WHERE `orderform`.`orderNo` = :orderNo AND `orderform`.`orderNo` = :orderNo");
+  $orders->bindValue(':memNo',$_SESSION['memNo']);
+  $orders->bindValue(':orderNo',$_POST['orderNo']);
+  $orders->execute();
+  $ordersRow = $orders->fetchObject(); 
+
+  if($ordersRow->order_status=="2"){
+    echo"已取消過";
+
+  }elseif($ordersRow->order_status=="1"){
+    echo"已出貨，無法取消";
+    
+  }else{
+
+    $orderform=$pdo->prepare("UPDATE `orderform` SET `order_status` = '2' WHERE `orderform`.`orderNo` = :orderNo AND `orderform`.`orderNo` = :orderNo ");
+    $orderform->bindValue(':memNo',$_SESSION['memNo']);
+    $orderform->bindValue(':orderNo',$_POST['orderNo']);
+    $orderform->execute();
+    echo"異動成功";
+  }
+  
+} catch (PDOException $e) {
+    $errMsg = $errMsg . "錯誤訊息: " . $e->getMessage() . "</br>";
+    $errMsg .= "錯誤行號: " . $e->getLine() . "<br>";
+    echo $errMsg;
+}
+
+?>
