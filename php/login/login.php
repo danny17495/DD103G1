01c5d5ecbdf7533d64fb2c6a0a45603510ed008a
DianaChang1 <dillkst123@gmail.com>
2019-11-21 02:56:15 +0800

    解衝突 喬

--- php/login/login.php
+++ php/login/login.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-<<<<<<< HEAD
 try{
         require_once("../connect.php");
         if(isset($_POST["user_id"]) && $_POST["user_id"] != ''){
@@ -30,38 +29,4 @@ try{
     // $errMsg .= "錯誤行號 : ".$e -> getLine(). "<br>";
 }
 
-=======
-
-try{
-    require_once("../connect.php");
-    if(isset($_POST["password"])){
-        $sql='select * from member where account=:account and password=:password;';
-        //  $sql='select * from user;';
-
-        $user=$pdo->prepare($sql);
-        $user->bindValue(':account',$_POST['account']);
-        $user->bindValue(':password',$_POST["password"]);
-        $user->execute();
-        if($user->rowCount() == 0){
-            echo 'loginError';
-        }else{
-            $userRow=$user->fetchAll(PDO::FETCH_ASSOC);
-            $_SESSION['memNo']=$userRow[0]['memNo'];
-            echo json_encode($userRow);
-        };
-    }else{
-        $sql='select * from user where memNo=:memNo;';
-        $user=$pdo->prepare($sql);
-        $user->bindValue(':memNo',$_SESSION['memNo']);
-        $user->execute();
-        $userRow=$user->fetchAll(PDO::FETCH_ASSOC);
-        echo json_encode($userRow);
-    }
-
-}catch(PDOException $e){
-    // echo $e->getMessage();
-    echo "sysError";
-
-}
->>>>>>> MYdev
 ?>
