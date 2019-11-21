faf0e4f2d535af2a39bd26fdd418693b30810726
mluvtw <>
2019-11-21 11:49:43 +0800

    孟 首頁明信片信封動態新增

--- js/indexTweenmax.js
+++ js/indexTweenmax.js
@@ -22,5 +22,27 @@ window.addEventListener("load", function () {
         triggerElement: '.indexSpotTree_start',
         //triggerHook: 0
     }).setClassToggle('.indexSpotTree', 'treeGrow').addTo(controller);
+
+    //首頁明信片打開
+    var postscene1 = new ScrollMagic.Scene({
+        triggerElement: '.postDIY',
+        reverse:false,
+    }).setClassToggle('.indexPost_trs1', 'trs1Down').addIndicators().addTo(controller);
+    
+    var postscene2 = new ScrollMagic.Scene({
+        triggerElement: '.postDIY',
+        reverse:false,
+    }).setClassToggle('.indexPost_trs2', 'trs2UP').addTo(controller);
+    
+    var postscene3 = new ScrollMagic.Scene({
+        triggerElement: '.postDIY',
+        reverse:false,
+    }).setClassToggle('.indexPost_trs3', 'trs3UP').addTo(controller);
+
+    var postsgirl = new ScrollMagic.Scene({
+        triggerElement: '.postDIY',
+        reverse:false,
+    }).setClassToggle('.postGirl', 'postGirlShow').addTo(controller);
+
         
 });
--- php/connect.php
+++ php/connect.php
@@ -1,6 +1,7 @@
 <?php
   $dsn = "mysql:host=localhost;port=3306;dbname=dd103g1;charset=utf8";
   $user = "root";
+  $password = "root";
   // $password = "da0919294452";
   // $user = "dd103g1";
   // $password = "UNIVERLK258";
