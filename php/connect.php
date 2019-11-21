bfede9d9b737d94291146f21d1108dcefc7eb497
DianaChang1 <dillkst123@gmail.com>
2019-11-21 11:59:38 +0800

    AA

--- php/connect.php
+++ php/connect.php
@@ -3,7 +3,6 @@
   $user = "root";
   // $password = "da0919294452";
   // $user = "dd103g1";
-  // $password = "UNIVERLK258";
   $options=array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
   $pdo = new PDO($dsn, $user, $password, $options);
 ?>
