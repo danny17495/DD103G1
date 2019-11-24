2f9f8c05614bb1428f2255a1098ccba29b9d5fcb
eric10101010 <erickaivalue@gmail.com>
2019-11-24 14:08:00 +0800

    修改資料

--- php/connect.php
+++ php/connect.php
@@ -1,7 +1,7 @@
 <?php
   $dsn = "mysql:host=localhost;port=3306;dbname=dd103g1;charset=utf8";
   $user = "root";
-  // $password = "da0919294452";
+  $password = "01258963";
   // $user = "dd103g1";
   $options=array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
   $pdo = new PDO($dsn, $user, $password, $options);
