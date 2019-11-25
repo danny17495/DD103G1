54385c9e74822d6eb332b06cda7ce53f2d8d2855
AbbyAbby33 <sk573129@gmail.com>
2019-11-25 17:21:28 +0800

    靜 1.更改php連結 2.新增connectg1.php

--- getPostcardNo.php
+++ getPostcardNo.php
@@ -2,11 +2,12 @@
 $errMsg ="";
 
 try {
-	$dsn = "mysql:host=localhost;port=3306;dbname=dd103g1;charset=utf8";
-	$user = "dd103g1";
-	$password = "dd103g1";
-	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
-	$pdo = new PDO($dsn, $user, $password, $options);
+	require_once("connectg1.php");
+	// $dsn = "mysql:host=localhost;port=3306;dbname=dd103g1;charset=utf8";
+	// $user = "dd103g1";
+	// $password = "dd103g1";
+	// $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
+	// $pdo = new PDO($dsn, $user, $password, $options);
 
 	//取得已有的明信片編號
 	$rowResult = 0;
