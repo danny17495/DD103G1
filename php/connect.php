cc3bcc545b249dba455efec5f9e6edf4315ed9de
AbbyAbby33 <sk573129@gmail.com>
2019-11-27 02:19:34 +0800

    靜 1.更改眾connect密碼 2.購物車深藍色衣服照片

--- php/connect.php
+++ php/connect.php
@@ -1,10 +1,10 @@
 <?php
   
   $dsn = "mysql:host=localhost;port=3306;dbname=dd103g1;charset=utf8";
-  // $user = "dd103g1";
-  // $password = "dd103g1";
-  $user = "root";
-  $password = "ddj1778";
+  $user = "dd103g1";
+  $password = "dd103g1";
+  // $user = "root";
+  // $password = "ddj1778";
   $options=array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
   $pdo = new PDO($dsn, $user, $password, $options);
 ?>
