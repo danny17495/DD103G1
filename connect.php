fe00e4d98e657ab5bc9b6621d7f25ff115980a49
mluvtw <>
2019-11-26 22:44:19 +0800

    孟

--- connect.php
+++ connect.php
@@ -2,17 +2,10 @@
   
   $dsn = "mysql:host=localhost;port=3306;dbname=dd103g1;charset=utf8";
 
-<<<<<<< HEAD
   // $password = "dd103g1";
   // $user = "dd103g1"; 
   $user = "root";
   $password = "root";
-=======
-  $password = "dd103g1";
-  $user = "dd103g1"; 
-  // $user = "root";
-  // $password = "ddj1778";
->>>>>>> b39cfb53570fb1289e2a76a9df77b44695ddc982
   $options=array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
   $pdo = new PDO($dsn, $user, $password, $options);
 ?>
--- game10.php
+++ game10.php
@@ -5,14 +5,9 @@ session_start();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-<<<<<<< HEAD
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>小遊戲</title>
-=======
-    <title>Document</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
->>>>>>> b39cfb53570fb1289e2a76a9df77b44695ddc982
     <link href="https://fonts.googleapis.com/css?family=Noto+Serif+TC&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
     <link rel="stylesheet" href="css/common.css">
