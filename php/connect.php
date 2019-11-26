97c0f0d4913a34f6ef87b407205bb3e4a9b57f70
danny17495 <danny17495@gmail.com>
2019-11-26 22:26:29 +0800

    修正MEMBER.PHP

--- php/connect.php
+++ php/connect.php
@@ -1,12 +1,7 @@
 <?php
   $dsn = "mysql:host=localhost;port=3306;dbname=dd103g1;charset=utf8";
-  $password = "da0919294452";
-  $user = "root";
-<<<<<<< HEAD
-  //$password = "dd103g1";
-   $password = "root";
-=======
->>>>>>> cc458fa155c2d78c7157c89e5e25a8d975054099
+  $user = "dd103g1";
+  $password = "dd103g1";
   $options=array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
   $pdo = new PDO($dsn, $user, $password, $options);
 ?>
