eec5e28a5f90100fb24e3cd154fe6d18502d9bc4
AbbyAbby33 <sk573129@gmail.com>
2019-11-26 21:54:23 +0800

    靜 1126

--- php/connect.php
+++ php/connect.php
@@ -1,7 +1,9 @@
 <?php
   $dsn = "mysql:host=localhost;port=3306;dbname=dd103g1;charset=utf8";
-  $password = "da0919294452";
-  $user = "root";
+  // $password = "dd103g1";
+  // $user = "dd103g1";
+  $user = "root";  
+  $password = "ddj1778";
   $options=array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
   $pdo = new PDO($dsn, $user, $password, $options);
 ?>
