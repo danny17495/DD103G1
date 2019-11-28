a4c92afcab943f649685207115ff140c393bc1b0
DianaChang1 <dillkst123@gmail.com>
2019-11-28 10:25:21 +0800

    123

--- backmanage/connect.php
+++ backmanage/connect.php
@@ -2,9 +2,9 @@
   $dsn = "mysql:host=localhost;port=3306;dbname=dd103g1;charset=utf8";
 
   
-  $user = "root";
+  $user = "dd103g1";
 
-  $password = "UNIVERLK258";  
+  $password = "dd103g1";  
   // $user = "dd103g1";
   $options=array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
   $pdo = new PDO($dsn, $user, $password, $options);
--- php/connect.php
+++ php/connect.php
@@ -1,8 +1,8 @@
 <?php
   $dsn = "mysql:host=localhost;port=3306;dbname=dd103g1;charset=utf8";
 
-  $user = "root";
-  $password = "UNIVERLK258";
+  $user = "dd103g1";
+  $password = "dd103g1";
 
   $options=array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
   $pdo = new PDO($dsn, $user, $password, $options);
